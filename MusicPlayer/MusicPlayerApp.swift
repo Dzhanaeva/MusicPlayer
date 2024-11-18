396e85fde7a663f1d4d7135649402c1dbc7aa2f0
Dzhanaeva <dzanaevagidayat@gmail.com>
2024-11-18 18:04:35 +0300

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Гидаят Джанаева on 18.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
